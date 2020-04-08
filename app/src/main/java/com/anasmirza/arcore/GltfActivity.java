61ece27c08da590eea3253b8f947a4d961813a08
Anasmirza20 <anas.ma854@gmail.com>
2020-04-09 00:17:10 +0530

    Initial commit

---
+++ app/src/main/java/com/anasmirza/arcore/GltfActivity.java
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.anasmirza.arcore;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.ArraySet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.google.android.filament.gltfio.Animator;
+import com.google.android.filament.gltfio.FilamentAsset;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.AugmentedFace;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Pose;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.Material;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.lang.ref.WeakReference;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * This is an example activity that uses the Sceneform UX package to make common AR tasks easier.
+ */
+public class GltfActivity extends AppCompatActivity {
+    private static final String TAG = GltfActivity.class.getSimpleName();
+    private static final double MIN_OPENGL_VERSION = 3.0;
+
+    private FaceArFragment arFragment;
+    private Renderable renderable;
+
+    private static class AnimationInstance {
+        Animator animator;
+        Long startTime;
+        float duration;
+        int index;
+
+        AnimationInstance(Animator animator, int index, Long startTime) {
+            this.animator = animator;
+            this.startTime = startTime;
+            this.duration = animator.getAnimationDuration(index);
+            this.index = index;
+        }
+    }
+
+    private final Set<AnimationInstance> animators = new ArraySet<>();
+
+    private final List<Color> colors =
+            Arrays.asList(
+                    new Color(0, 0, 0, 1),
+                    new Color(1, 0, 0, 1),
+                    new Color(0, 1, 0, 1),
+                    new Color(0, 0, 1, 1),
+                    new Color(1, 1, 0, 1),
+                    new Color(0, 1, 1, 1),
+                    new Color(1, 0, 1, 1),
+                    new Color(1, 1, 1, 1));
+    private int nextColor = 0;
+
+    @Override
+    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
+    // CompletableFuture requires api level 24
+    // FutureReturnValueIgnored is not valid
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return;
+        }
+
+        setContentView(R.layout.activity_ux);
+        arFragment = (FaceArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        WeakReference<GltfActivity> weakActivity = new WeakReference<>(this);
+
+        ModelRenderable.builder()
+                .setSource(this, R.raw.fox_face)
+                .setIsFilamentGltf(true)
+                .build()
+                .thenAccept(
+                        modelRenderable -> {
+                            GltfActivity activity = weakActivity.get();
+                            if (activity != null) {
+                                activity.renderable = modelRenderable;
+                            }
+                        })
+                .exceptionally(
+                        throwable -> {
+                            Toast toast =
+                                    Toast.makeText(this, "Unable to load Tiger renderable", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                            return null;
+                        });
+
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    if (renderable == null) {
+                        return;
+                    }
+
+                    // Create the Anchor.
+                    Anchor anchor = hitResult.createAnchor();
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+                    anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                    // Create the transformable model and add it to the anchor.
+                    TransformableNode model = new TransformableNode(arFragment.getTransformationSystem());
+                    model.setParent(anchorNode);
+                    model.setRenderable(renderable);
+                    model.select();
+
+                    FilamentAsset filamentAsset = model.getRenderableInstance().getFilamentAsset();
+                    if (filamentAsset.getAnimator().getAnimationCount() > 0) {
+                        animators.add(new AnimationInstance(filamentAsset.getAnimator(), 0, System.nanoTime()));
+                    }
+
+                    Color color = colors.get(nextColor);
+                    nextColor++;
+                    for (int i = 0; i < renderable.getSubmeshCount(); ++i) {
+                        Material material = renderable.getMaterial(i);
+                        material.setFloat4("baseColorFactor", color);
+                    }
+                });
+
+        arFragment
+                .getArSceneView()
+                .getScene()
+                .addOnUpdateListener(
+                        frameTime -> {
+             /* Long time = System.nanoTime();
+              for (AnimationInstance animator : animators) {
+                animator.animator.applyAnimation(
+                    animator.index,
+                    (float) ((time - animator.startTime) / (double) SECONDS.toNanos(1))
+                        % animator.duration);
+                animator.animator.updateBoneMatrices();
+              }*/
+                            Collection<AugmentedFace> faces = arFragment.getArSceneView().getSession().getAllTrackables(AugmentedFace.class);
+
+                            for (AugmentedFace face : faces) {
+                                if (face.getTrackingState() == TrackingState.TRACKING) {
+                                    // UVs and indices can be cached as they do not change during the session.
+                                    FloatBuffer uvs = face.getMeshTextureCoordinates();
+                                    ShortBuffer indices = face.getMeshTriangleIndices();
+                                    // Center and region poses, mesh vertices, and normals are updated each frame.
+                                    Pose facePose = face.getCenterPose();
+                                    FloatBuffer faceVertices = face.getMeshVertices();
+                                    FloatBuffer faceNormals = face.getMeshNormals();
+                                    // Render the face using these values with OpenGL
+                                }
+                            }
+                        });
+    }
+
+    /**
+     * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
+     * on this device.
+     *
+     * <p>Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
+     *
+     * <p>Finishes the activity if Sceneform can not run
+     */
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        String openGlVersionString =
+                ((ActivityManager) Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE)))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/anasmirza/arcore/MainActivity.java
@@ -0,0 +1,16 @@
+package com.anasmirza.arcore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
